bea6f725c9effd00c94f677a8e1e88f209d4f2ea
pratik <pratikpakhale20@gmail.com>
2024-04-24 05:02:46 +0530

    Update preprocess.py and config.json

--- ai/utils/preprocess.py
+++ ai/utils/preprocess.py
@@ -7,10 +7,12 @@ def extract_json(input_string):
 
     try:
         return json.loads(input_string)
-    except:
+    except json.JSONDecodeError:
         json_matches = re.findall(r'{(?:[^{}\\]|\\.)*}', input_string)
         if json_matches:
-            return json.loads(json_matches[0])
-
-    
-    return None
+            try:
+                return json.loads(json_matches[0])
+            except json.JSONDecodeError:
+                return None
+        else:
+            return None
